7a125301d8ad4a721466efa6dab1f37f2350f9b7
Naveen_17 <83273127+Naveenkumar705@users.noreply.github.com>
2023-07-10 22:10:30 +0530

    Add files via upload

---
+++ bank.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select * from customerdetial
+LIMIT 0, 1000
+
+-- Date: 2023-07-10 22:03
+*/
+INSERT INTO `` (`Acc_no`,`Acc_name`,`pin_no`,`pan_no`,`balance`,`address`,`acc_type`) VALUES (1235,'vasanth',1507,'vas123',250090,'chennai','Joint');
+INSERT INTO `` (`Acc_no`,`Acc_name`,`pin_no`,`pan_no`,`balance`,`address`,`acc_type`) VALUES (12345,'naveen',1511,'nav123',25000,'salem','benificiary');
+INSERT INTO `` (`Acc_no`,`Acc_name`,`pin_no`,`pan_no`,`balance`,`address`,`acc_type`) VALUES (12445,'mani',1410,'man123',50000,'salem','benificiary');
+INSERT INTO `` (`Acc_no`,`Acc_name`,`pin_no`,`pan_no`,`balance`,`address`,`acc_type`) VALUES (12675,'gokul',1212,'gok123',250000,'namakkal','benificiary');
